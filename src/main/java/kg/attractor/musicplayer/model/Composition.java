b1f4e5594202155028d286dae09adc50a9a695e3
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-12 04:38:08 +0600

    added compostion class, made it @document, makecompositions method

---
+++ src/main/java/kg/attractor/musicplayer/model/Composition.java
@@ -0,0 +1,41 @@
+package kg.attractor.musicplayer.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+@Document(collection = "compositions")
+@Data
+public class Composition {
+
+    @Id
+    private String id;
+    private String name;
+    private LocalDate releaseDate;
+    private static List<Composition> compositions = makeComposition();
+
+    public Composition(String name, LocalDate releaseDate) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.releaseDate = releaseDate;
+    }
+
+    private static List<Composition> makeComposition(){
+        List<Composition> compositions = new LinkedList<>();
+        LocalDate localDate  = LocalDate.now();
+        Random r = new Random();
+        compositions.add(new Composition("Warrior",localDate.minusYears(r.nextInt(10)+1)));
+        compositions.add(new Composition("Darkness",localDate.minusYears(r.nextInt(10)+1)));
+        compositions.add(new Composition("Madness",localDate.minusYears(r.nextInt(10)+1)));
+        return  compositions;
+    }
+    public static List<Composition> getCompositions(){
+        return compositions;
+    }
+}
