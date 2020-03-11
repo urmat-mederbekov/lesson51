a0ddf918042dea986b1237f84911b2e36b1d23f8
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-12 04:39:18 +0600

    added composition repository

---
+++ src/main/java/kg/attractor/musicplayer/model/CompositionRepository.java
@@ -0,0 +1,7 @@
+package kg.attractor.musicplayer.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompositionRepository extends CrudRepository<Composition, String> {
+}
+
